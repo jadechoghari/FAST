d35dcd78a767367b2caff29dab5230637e6e011b
Niels Rogge <niels.rogge1@gmail.com>
2023-08-22 17:31:06 +0200

    More fixes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
-from .pan import PAN
+# from .pan import PAN
 from .fast import FAST
-from .psenet import PSENet
+# from .psenet import PSENet
 from .builder import build_model
 
-__all__ = ['PAN', 'PSENet', 'FAST']
+# __all__ = ['PAN', 'PSENet', 'FAST']
+
+__all__ = ['FAST']
