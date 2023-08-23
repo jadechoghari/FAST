742a4806e407a870d3660b86f51b2f370bd9836c
Niels Rogge <niels.rogge1@gmail.com>
2023-08-23 09:28:55 +0200

    More fixes

--- inference.py
+++ inference.py
@@ -59,9 +59,12 @@ def test(model, cfg):
 
     transform = SquarePadResizeNorm(img_size=512, norm_mean=(0.485, 0.456, 0.406), norm_std=(0.229, 0.224, 0.225))
     imgs = transform(image)[0]
+    batch_size = imgs.shape[0]
     data["imgs"] = imgs
-    img_metas = {"org_img_size": [np.array(image).shape[:2]],
-                 "img_size": [imgs.shape[:2]]}
+    img_metas = {'filename': [None for i in range(batch_size)],
+                'org_img_size': torch.ones((batch_size,2)).long()*512,
+                'img_size': torch.ones((batch_size,2)).long()*512,
+    }
     data["img_metas"] = img_metas
 
     if not args.cpu:
